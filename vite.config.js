6545dcb1100a164924d297e6f5b48f710cc278f8
Titus Lim <tituslhy@gmail.com>
2026-06-24 13:11:54 +0800

    add progressive web app mode for app to run offline

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,ico,jpg,jpeg,png,webp,json}'],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
